2b75f7ef7675a4136be7fda5a1118ec29749de6d
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-06-06 20:46:36 +0300

    Add project files.

---
+++ ITMO.Python-Labs.02/ITMO.Python_Labs.02.py
@@ -0,0 +1,14 @@
+# Лабораторная 2
+string1 = " This is a string. "
+string2 = " This is another string. "
+a = string1 + string2
+print(a)
+print(len(string1))
+print(string1.title())
+print(string1.lower())
+print(string1.upper())
+print(string1.rstrip())
+print(string1.lstrip())
+print(string1.strip())
+print(string1.strip('0'))
+
