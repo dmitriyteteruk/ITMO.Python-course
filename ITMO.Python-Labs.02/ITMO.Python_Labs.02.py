529621bce0bfcb9cc8c803e77c08f20c6675af78
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-06-06 20:56:52 +0300

    добавлен пример задачи 6

--- ITMO.Python-Labs.02/ITMO.Python_Labs.02.py
+++ ITMO.Python-Labs.02/ITMO.Python_Labs.02.py
@@ -1,8 +1,18 @@
 # Лабораторная 2
+# Задача 1 
+print("----- Задача 01 -----")
 string1 = " This is a string. "
+print(string1)
 string2 = " This is another string. "
+print(string2)
+
+print("----- Задача 02 -----")
+# Задача 2
 a = string1 + string2
 print(a)
+
+# Задача 3
+print("----- Задача 03 -----")
 print(len(string1))
 print(string1.title())
 print(string1.lower())
@@ -12,3 +22,20 @@ print(string1.lstrip())
 print(string1.strip())
 print(string1.strip('0'))
 
+# Задача 4
+print("----- Задача 04 -----")
+string3 = string1[6]
+print(string3)
+
+# Задача 5
+print("----- Задача 05 -----")
+string3 = string1[1:8]
+# Задача 6
+print("----- Задача 06 -----")
+print(string3)
+string3 = string1[3:]
+print(string3)
+string3 = string1[:8]
+print(string3)
+string3 = string1[1:3:8]
+print(string3)
